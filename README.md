92f853932bac8dbe9da564e20d8dd711713c9c3c
Diego Lopes <diegoplopes5@gmail.com>
2022-07-11 18:28:49 -0300

    chore: setup project structure

---
+++ README.md
@@ -0,0 +1,10 @@
+# Video Catalog Admin
+
+## Description
+
+API to manage the catalog of a streaming platform
+
+## Technologies
+
+* ```java: 17.0.2```
+* ```gradle: 7.4```
---
+++ domain/src/test/java/com/codeflix/admin/video/catalog/domain/CategoryTest.java
@@ -0,0 +1,13 @@
+package com.codeflix.admin.video.catalog.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CategoryTest {
+
+    @Test
+    public void testNewCategory() {
+        Assertions.assertNotNull(new Category());
+
+    }
+}
