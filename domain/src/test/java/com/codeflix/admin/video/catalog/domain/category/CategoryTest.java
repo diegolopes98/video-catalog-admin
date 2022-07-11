fff9f881d406e96a1409f926113e576ca74f3625
Diego Lopes <diegoplopes5@gmail.com>
2022-07-11 19:13:42 -0300

    test(domain): add category aggregate root

---
+++ domain/src/test/java/com/codeflix/admin/video/catalog/domain/category/CategoryTest.java
@@ -0,0 +1,23 @@
+package com.codeflix.admin.video.catalog.domain.category;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CategoryTest {
+
+    @Test
+    public void givenValidParams_whenCallNewCategory_thenInstantiateACategory() {
+        final var expectedName = "Movies";
+        final var expectedDescription = "Movies category";
+        final var expectedIsActive =  true;
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+    }
+}
