324d55e08243a2d7929d901c51996c74534025b4
Diego Lopes <diegoplopes5@gmail.com>
2022-07-12 17:28:19 -0300

    fix(infrastructure): remove usecase creation from main

--- infrastructure/src/main/java/com/codeflix/admin/video/catalog/infrastructure/Main.java
+++ infrastructure/src/main/java/com/codeflix/admin/video/catalog/infrastructure/Main.java
@@ -1,11 +1,7 @@
 package com.codeflix.admin.video.catalog.infrastructure;
 
-import com.codeflix.admin.video.catalog.application.UseCase;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-        System.out.println(new UseCase().execute());
     }
 }
