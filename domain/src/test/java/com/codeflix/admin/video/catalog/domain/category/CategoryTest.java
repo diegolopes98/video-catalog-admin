4d237e1d7e09f79565db2797e9bdb42a7e855988
Diego Lopes <diegoplopes5@gmail.com>
2022-07-12 13:42:10 -0300

    test(domain): add category validator

--- domain/src/test/java/com/codeflix/admin/video/catalog/domain/category/CategoryTest.java
+++ domain/src/test/java/com/codeflix/admin/video/catalog/domain/category/CategoryTest.java
@@ -1,5 +1,7 @@
 package com.codeflix.admin.video.catalog.domain.category;
 
+import com.codeflix.admin.video.catalog.domain.exceptions.DomainException;
+import com.codeflix.admin.video.catalog.domain.validation.handler.ThrowsValidationHandler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +21,25 @@ public class CategoryTest {
         Assertions.assertNotNull(actualCategory.getCreatedAt());
         Assertions.assertNotNull(actualCategory.getUpdatedAt());
         Assertions.assertNull(actualCategory.getDeletedAt());
+    }
+
+    @Test
+    public void givenInvalidNullName_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final String expectedName = null;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedDescription = "Movies category";
+        final var expectedIsActive =  true;
+        final var actualCategory =
+                Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualException =
+                Assertions.assertThrows(
+                        DomainException.class,
+                        () -> actualCategory.validate(new ThrowsValidationHandler())
+                );
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
     }
 }
