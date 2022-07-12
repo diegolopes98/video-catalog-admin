fec22335cb38f44489451543e2df7577a276443d
Diego Lopes <diegoplopes5@gmail.com>
2022-07-12 13:51:10 -0300

    refactor(domain): add NoStackTraceException and DomainException messages

--- domain/src/main/java/com/codeflix/admin/video/catalog/domain/exceptions/DomainException.java
+++ domain/src/main/java/com/codeflix/admin/video/catalog/domain/exceptions/DomainException.java
@@ -4,16 +4,22 @@ import com.codeflix.admin.video.catalog.domain.validation.Error;
 
 import java.util.List;
 
-public class DomainException extends RuntimeException {
+public class DomainException extends NoStackTraceException {
+
+    private static final String MULTIPLE_ERRORS_MESSAGE = "the domain validation returned some errors";
     private final List<Error> errors;
 
-    private DomainException(final List<Error> anErrorList) {
-        super("", null, true, false);
+    private DomainException(final String aMessage, final List<Error> anErrorList) {
+        super(aMessage);
         this.errors = anErrorList;
     }
 
+    public static DomainException with(final Error anError) {
+        return new DomainException(anError.message(), List.of(anError));
+    }
+
     public static DomainException with(final List<Error> anErrorList) {
-        return new DomainException(anErrorList);
+        return new DomainException(MULTIPLE_ERRORS_MESSAGE, anErrorList);
     }
 
     public List<Error> getErrors() {
