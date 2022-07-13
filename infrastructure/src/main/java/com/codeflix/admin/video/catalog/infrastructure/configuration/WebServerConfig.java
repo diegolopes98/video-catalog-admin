55399d31cb4aed82c945b6e7f7696b33515d6f06
Diego Lopes <diegoplopes5@gmail.com>
2022-07-13 20:29:03 -0300

    chore(infrastructure): add spring configuration

---
+++ infrastructure/src/main/java/com/codeflix/admin/video/catalog/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.codeflix.admin.video.catalog.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.codeflix.admin.video.catalog")
+public class WebServerConfig {
+}
