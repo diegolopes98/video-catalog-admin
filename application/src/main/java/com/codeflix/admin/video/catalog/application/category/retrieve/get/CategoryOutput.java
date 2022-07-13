61f0c5f05dc8fdf1478b136e99de337a28a7a436
Diego Lopes <diegoplopes5@gmail.com>
2022-07-12 23:18:12 -0300

    feat(application): add get by id use case

---
+++ application/src/main/java/com/codeflix/admin/video/catalog/application/category/retrieve/get/CategoryOutput.java
@@ -0,0 +1,28 @@
+package com.codeflix.admin.video.catalog.application.category.retrieve.get;
+
+import com.codeflix.admin.video.catalog.domain.category.Category;
+import com.codeflix.admin.video.catalog.domain.category.CategoryID;
+
+import java.time.Instant;
+
+public record CategoryOutput(
+        CategoryID id,
+        String name,
+        String description,
+        boolean isActive,
+        Instant createdAt,
+        Instant updatedAt,
+        Instant deletedAt
+) {
+    public static CategoryOutput from(final Category aCategory) {
+        return new CategoryOutput(
+                aCategory.getId(),
+                aCategory.getName(),
+                aCategory.getDescription(),
+                aCategory.isActive(),
+                aCategory.getCreatedAt(),
+                aCategory.getUpdatedAt(),
+                aCategory.getDeletedAt()
+        );
+    }
+}
---
+++ application/src/main/java/com/codeflix/admin/video/catalog/application/category/retrieve/get/GetCategoryByIdUseCase.java
@@ -0,0 +1,6 @@
+package com.codeflix.admin.video.catalog.application.category.retrieve.get;
+
+import com.codeflix.admin.video.catalog.application.UseCase;
+
+public abstract class GetCategoryByIdUseCase extends UseCase<String, CategoryOutput> {
+}
