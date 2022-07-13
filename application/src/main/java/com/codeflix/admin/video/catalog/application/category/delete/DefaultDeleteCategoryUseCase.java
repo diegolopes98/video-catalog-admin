6c80dfc4c7c6de17aea24a464a8b32d87228f7e7
Diego Lopes <diegoplopes5@gmail.com>
2022-07-12 22:45:56 -0300

    feat(application): add delete use case

---
+++ application/src/main/java/com/codeflix/admin/video/catalog/application/category/delete/DefaultDeleteCategoryUseCase.java
@@ -0,0 +1,21 @@
+package com.codeflix.admin.video.catalog.application.category.delete;
+
+import com.codeflix.admin.video.catalog.domain.category.CategoryGateway;
+import com.codeflix.admin.video.catalog.domain.category.CategoryID;
+
+import java.util.Objects;
+
+public class DefaultDeleteCategoryUseCase extends DeleteCategoryUseCase {
+
+    private final CategoryGateway categoryGateway;
+
+    public DefaultDeleteCategoryUseCase(final CategoryGateway categoryGateway) {
+        Objects.requireNonNull(categoryGateway);
+        this.categoryGateway = categoryGateway;
+    }
+
+    @Override
+    public void execute(String anId) {
+        this.categoryGateway.deleteById(CategoryID.from(anId));
+    }
+}
---
+++ application/src/main/java/com/codeflix/admin/video/catalog/application/category/delete/DeleteCategoryUseCase.java
@@ -0,0 +1,6 @@
+package com.codeflix.admin.video.catalog.application.category.delete;
+
+import com.codeflix.admin.video.catalog.application.UnitUseCase;
+
+public abstract class DeleteCategoryUseCase extends UnitUseCase<String> {
+}
