6e82b2e0b4491ab1968f012dfc58fc43d07316e3
Diego Lopes <diegoplopes5@gmail.com>
2022-07-13 22:26:38 -0300

    feat(infrastructure): add jpa entity to category aggregate

--- domain/src/main/java/com/codeflix/admin/video/catalog/domain/category/Category.java
+++ domain/src/main/java/com/codeflix/admin/video/catalog/domain/category/Category.java
@@ -7,122 +7,142 @@ import java.time.Instant;
 
 public class Category extends AggregateRoot<CategoryID> {
 
-    private String name;
-    private String description;
-    private boolean active;
-    private Instant createdAt;
-    private Instant updatedAt;
-    private Instant deletedAt;
+	private String name;
+	private String description;
+	private boolean active;
+	private Instant createdAt;
+	private Instant updatedAt;
+	private Instant deletedAt;
 
-    private Category(
-            final CategoryID anId,
-            final String aName,
-            final String aDescription,
-            final boolean isActive,
-            final Instant aCreationDate,
-            final Instant aUpdateDate,
-            final Instant aDeleteDate
-    ) {
-        super(anId);
-        this.name = aName;
-        this.description = aDescription;
-        this.active = isActive;
-        this.createdAt = aCreationDate;
-        this.updatedAt = aUpdateDate;
-        this.deletedAt = aDeleteDate;
-    }
+	private Category(
+			final CategoryID anId,
+			final String aName,
+			final String aDescription,
+			final boolean isActive,
+			final Instant aCreationDate,
+			final Instant aUpdateDate,
+			final Instant aDeleteDate
+	) {
+		super(anId);
+		this.name = aName;
+		this.description = aDescription;
+		this.active = isActive;
+		this.createdAt = aCreationDate;
+		this.updatedAt = aUpdateDate;
+		this.deletedAt = aDeleteDate;
+	}
 
-    public static Category newCategory(
-            final String aName,
-            final String aDescription,
-            final boolean isActive
-    ) {
-        final var id = CategoryID.unique();
-        final var now = Instant.now();
-        final var deletedAt = isActive ? null : now;
-        return new Category(
-                id,
-                aName,
-                aDescription,
-                isActive,
-                now,
-                now,
-                deletedAt
-        );
-    }
+	public static Category newCategory(
+			final String aName,
+			final String aDescription,
+			final boolean isActive
+	) {
+		final var id = CategoryID.unique();
+		final var now = Instant.now();
+		final var deletedAt = isActive ? null : now;
+		return new Category(
+				id,
+				aName,
+				aDescription,
+				isActive,
+				now,
+				now,
+				deletedAt
+		);
+	}
 
-    public static Category from(Category aCategory) {
-        return new Category(
-                aCategory.getId(),
-                aCategory.getName(),
-                aCategory.getDescription(),
-                aCategory.isActive(),
-                aCategory.getCreatedAt(),
-                aCategory.getUpdatedAt(),
-                aCategory.getDeletedAt()
-        );
-    }
+	public static Category from(Category aCategory) {
+		return with(
+				aCategory.getId(),
+				aCategory.getName(),
+				aCategory.getDescription(),
+				aCategory.isActive(),
+				aCategory.getCreatedAt(),
+				aCategory.getUpdatedAt(),
+				aCategory.getDeletedAt()
+		);
+	}
 
-    @Override
-    public void validate(final ValidationHandler handler) {
-        new CategoryValidator(this, handler).validate();
-    }
+	public static Category with(
+			final CategoryID anId,
+			final String aName,
+			final String aDescription,
+			final boolean isActive,
+			final Instant aCreationDate,
+			final Instant anUpdateDate,
+			final Instant aDeleteDate
+	) {
+		return new Category(
+				anId,
+				aName,
+				aDescription,
+				isActive,
+				aCreationDate,
+				anUpdateDate,
+				aDeleteDate
+		);
+	}
 
-    public Category activate() {
-        final var now = Instant.now();
-        this.deletedAt = null;
-        this.active = true;
-        this.updatedAt = now;
-        return this;
-    }
+	@Override
+	public void validate(final ValidationHandler handler) {
+		new CategoryValidator(this, handler).validate();
+	}
 
-    public Category deactivate() {
-        final var now = Instant.now();
-        if (getDeletedAt() == null) {
-            this.deletedAt = now;
-        }
-        this.active = false;
-        this.updatedAt = now;
-        return this;
-    }
+	public Category activate() {
+		final var now = Instant.now();
+		this.deletedAt = null;
+		this.active = true;
+		this.updatedAt = now;
+		return this;
+	}
 
-    public Category update(final String aName, final String aDescription, final boolean isActive ) {
-        if (isActive) {
-            activate();
-        } else {
-            deactivate();
-        }
-        this.name = aName;
-        this.description = aDescription;
-        this.updatedAt = Instant.now();
-        return this;
-    }
+	public Category deactivate() {
+		final var now = Instant.now();
+		if (getDeletedAt() == null) {
+			this.deletedAt = now;
+		}
+		this.active = false;
+		this.updatedAt = now;
+		return this;
+	}
 
-    public CategoryID getId() {
-        return id;
-    }
+	public Category update(final String aName, final String aDescription, final boolean isActive) {
+		if (isActive) {
+			activate();
+		} else {
+			deactivate();
+		}
+		this.name = aName;
+		this.description = aDescription;
+		this.updatedAt = Instant.now();
+		return this;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public CategoryID getId() {
+		return id;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public boolean isActive() {
-        return active;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
+	public boolean isActive() {
+		return active;
+	}
 
-    public Instant getUpdatedAt() {
-        return updatedAt;
-    }
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
 
-    public Instant getDeletedAt() {
-        return deletedAt;
-    }
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public Instant getDeletedAt() {
+		return deletedAt;
+	}
 }
