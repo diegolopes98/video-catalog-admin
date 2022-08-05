c13b5a1b0bc39278782e9c7be6af7ecdc20ee56f
Diego Lopes <diegoplopes5@gmail.com>
2022-08-05 17:38:44 -0300

    test(domain): add genre aggregate

---
+++ domain/src/test/java/com/codeflix/admin/video/catalog/domain/genre/GenreTest.java
@@ -0,0 +1,81 @@
+package com.codeflix.admin.video.catalog.domain.genre;
+
+import com.codeflix.admin.video.catalog.domain.exceptions.NotificationException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GenreTest {
+
+    @Test
+    public void givenValidParams_whenCallNewGenre_thenShouldInstantiateAGenre() {
+        final var expectedName = "Action";
+        final var expectedIsActive = true;
+        final var expectedCategoriesSize = 0;
+
+        final var actualGenre = Genre.newGenre(expectedName, expectedIsActive);
+
+        Assertions.assertNotNull(actualGenre);
+        Assertions.assertNotNull(actualGenre.getId());
+        Assertions.assertEquals(expectedName, actualGenre.getName());
+        Assertions.assertEquals(expectedIsActive, actualGenre.isActive());
+        Assertions.assertEquals(expectedCategoriesSize, actualGenre.getCategories().size());
+        Assertions.assertNotNull(actualGenre.getCreatedAt());
+        Assertions.assertNotNull(actualGenre.getUpdatedAt());
+        Assertions.assertNull(actualGenre.getDeletedAt());
+    }
+
+    @Test
+    public void givenInvalidNullName_whenCallNewGenreAndValidate_thenShouldReceiveAnError() {
+        final String expectedName = null;
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMsg = "'name' should not be null";
+
+        final var actualException = Assertions.assertThrows(
+                NotificationException.class,
+                () -> Genre.newGenre(expectedName, expectedIsActive)
+        );
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMsg, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenInvalidEmptyName_whenCallNewGenreAndValidate_thenShouldReceiveAnError() {
+        final var expectedName = "   ";
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMsg = "'name' should not be empty";
+
+        final var actualException = Assertions.assertThrows(
+                NotificationException.class,
+                () -> Genre.newGenre(expectedName, expectedIsActive)
+        );
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMsg, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenInvalidNameWithLengthGreaterThan255_whenCallNewGenreAndValidate_thenShouldReceiveAnError() {
+        final var expectedName = """
+                     O cuidado em identificar pontos críticos na consulta aos diversos militantes é uma das consequências
+                     dos conhecimentos estratégicos para atingir a excelência. Podemos já vislumbrar o modo pelo qual a
+                     determinação clara de objetivos facilita a criação das direções preferenciais no sentido do progresso.
+                """;
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMsg = "'name' must be between 1 and 255 characters";
+
+        final var actualException = Assertions.assertThrows(
+                NotificationException.class,
+                () -> Genre.newGenre(expectedName, expectedIsActive)
+        );
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMsg, actualException.getErrors().get(0).message());
+    }
+}
