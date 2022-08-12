68712d8a6019d21b8357ffe4996499d524ed994c
Diego Lopes <diegoplopes5@gmail.com>
2022-08-12 14:17:34 -0300

    test(application): add create genre use case happy path

---
+++ application/src/test/java/com/codeflix/admin/video/catalog/application/genre/create/CreateGenreUseCaseTest.java
@@ -0,0 +1,75 @@
+package com.codeflix.admin.video.catalog.application.genre.create;
+
+import com.codeflix.admin.video.catalog.domain.category.CategoryGateway;
+import com.codeflix.admin.video.catalog.domain.category.CategoryID;
+import com.codeflix.admin.video.catalog.domain.genre.GenreGateway;
+import com.codeflix.admin.video.catalog.domain.genre.GenreID;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class CreateGenreUseCaseTest {
+
+    @InjectMocks
+    private DefaultCreateGenreUseCase useCase;
+
+    @Mock
+    private GenreGateway genreGateway;
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
+    @BeforeEach
+    public void cleanUp() {
+        Mockito.reset(genreGateway);
+        Mockito.reset(categoryGateway);
+    }
+
+    @Test
+    public void givenAValidCommand_whenCallsCreateGenre_thenShouldReturnGenreId() {
+        final var expectedName = "Action";
+        final var expectedIsActive = true;
+        final var expectedCategories = List.<CategoryID>of();
+
+        final var aCommand =
+                CreateGenreCommand.with(expectedName, expectedIsActive, asString(expectedCategories));
+
+        when(genreGateway.create(any()))
+                .thenAnswer(returnsFirstArg());
+
+        final var actualOutput = useCase.execute(aCommand);
+
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.id());
+        Mockito.verify(genreGateway, times(1)).create(argThat(
+                aGenre -> Objects.equals(expectedName, aGenre.getName())
+                        && Objects.equals(expectedIsActive, aGenre.isActive())
+                        && Objects.equals(expectedCategories, aGenre.getCategories())
+                        && Objects.nonNull(aGenre.getId())
+                        && Objects.nonNull(aGenre.getCreatedAt())
+                        && Objects.nonNull(aGenre.getUpdatedAt())
+                        && Objects.isNull(aGenre.getDeletedAt())
+        ));
+    }
+
+    private List<String> asString(List<CategoryID> categories) {
+        return categories.stream()
+                .map(id -> id.getValue())
+                .toList();
+    }
+}
