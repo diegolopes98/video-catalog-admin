a1cffae59e947def4e0cb5c42e28a219ace83569
Diego Lopes <diegoplopes5@gmail.com>
2022-08-30 20:27:46 -0300

    feat(application): add delete Genre use case

---
+++ application/src/main/java/com/codeflix/admin/video/catalog/application/genre/delete/DefaultDeleteGenreUseCase.java
@@ -0,0 +1,20 @@
+package com.codeflix.admin.video.catalog.application.genre.delete;
+
+import com.codeflix.admin.video.catalog.domain.genre.GenreGateway;
+import com.codeflix.admin.video.catalog.domain.genre.GenreID;
+
+import java.util.Objects;
+
+public class DefaultDeleteGenreUseCase extends DeleteGenreUseCase {
+
+    private final GenreGateway genreGateway;
+
+    public DefaultDeleteGenreUseCase(final GenreGateway genreGateway) {
+        this.genreGateway = Objects.requireNonNull(genreGateway);
+    }
+
+    @Override
+    public void execute(String anId) {
+        this.genreGateway.deleteById(GenreID.from(anId));
+    }
+}
---
+++ application/src/main/java/com/codeflix/admin/video/catalog/application/genre/delete/DeleteGenreUseCase.java
@@ -0,0 +1,6 @@
+package com.codeflix.admin.video.catalog.application.genre.delete;
+
+import com.codeflix.admin.video.catalog.application.UnitUseCase;
+
+public abstract class DeleteGenreUseCase extends UnitUseCase<String> {
+}
