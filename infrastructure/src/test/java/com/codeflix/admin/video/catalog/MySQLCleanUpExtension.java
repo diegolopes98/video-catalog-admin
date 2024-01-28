40da751234decad23138c5a909352218c5d19fd8
Diego Lopes <diegopereiralopes98@gmail.com>
2024-01-28 16:27:26 -0300

    test(infra): add CastMemberRepository to MySQLCleanUpExtension

--- infrastructure/src/test/java/com/codeflix/admin/video/catalog/MySQLCleanUpExtension.java
+++ infrastructure/src/test/java/com/codeflix/admin/video/catalog/MySQLCleanUpExtension.java
@@ -1,10 +1,10 @@
 package com.codeflix.admin.video.catalog;
 
+import com.codeflix.admin.video.catalog.infrastructure.castmember.persistence.CastMemberRepository;
 import com.codeflix.admin.video.catalog.infrastructure.category.persistence.CategoryRepository;
 import com.codeflix.admin.video.catalog.infrastructure.genre.persistence.GenreRepository;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -17,6 +17,7 @@ public class MySQLCleanUpExtension implements BeforeEachCallback {
 		final var appContext = SpringExtension.getApplicationContext(context);
 
 		cleanUp(List.of(
+				appContext.getBean(CastMemberRepository.class),
 				appContext.getBean(GenreRepository.class),
 				appContext.getBean(CategoryRepository.class)
 		));
